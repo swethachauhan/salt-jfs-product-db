d675f0a93ea6beb4f2c139dfba6667d32e414729
salt-dev <swetha.chauhan0001@gmail.com>
2022-10-05 10:31:16 +0200

    server connection changes added

---
+++ src/main/resources/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO product_group (name) values ('Food');
+INSERT INTO product_group (name) values ('Toys');
+INSERT INTO product_group (name) values ('Sports Gear');
+
+-- INSERT INTO product (name, description, price, pg_id) VALUES ('Pizza', 'Junk', 100, 1);
+-- INSERT INTO product (name, description, price, pg_id) VALUES ('Burger', '', 80, 1);
+-- INSERT INTO product (name, description, price, pg_id) VALUES ('Shoes', 'Fast', 420, 2);
+-- INSERT INTO product (name, description, price, pg_id) VALUES ('Flute', '', 50, 3);
+-- INSERT INTO product (name, description, price, pg_id) VALUES ('PS5', 'Best toy', 6000, 3);
+
